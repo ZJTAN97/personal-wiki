7bddc528e56a13d6ba86370000a28c1c3ab8c296
ZJ <zhijietan97@gmail.com>
2026-01-01 18:56:02 +0800

    chore: update titles and add documentation for Two Sum, Move Zeroes, and Guess Number Higher or Lower

--- test.ts
+++ test.ts
@@ -1,11 +1,23 @@
-function reverseWords(s: string): string {
-  return s
-    .split(" ")
-    .filter((word) => word !== "")
-    .reverse()
-    .join(" ");
+const nums = [0, 1, 0, 3, 12];
+const nums2 = [0, 0, 1];
+
+function moveZeroes(nums: number[]): void {
+  let leftPointer = 0;
+  let temp: number;
+
+  for (let rightPointer = 0; rightPointer < nums.length; rightPointer++) {
+    if (nums[rightPointer] !== 0) {
+      temp = nums[leftPointer];
+      nums[leftPointer] = nums[rightPointer];
+      nums[rightPointer] = temp;
+
+      leftPointer++;
+    }
+  }
 }
 
-reverseWords("the sky is blue");
-reverseWords("  hello world  ");
-reverseWords("a good  example");
+moveZeroes(nums);
+// moveZeroes(nums2);
+
+console.log(nums);
+// console.log(nums2);
