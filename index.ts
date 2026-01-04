8ee56ca70c789f976df47e12c06645edea4dac1a
ZJ <zhijietan97@gmail.com>
2026-01-04 23:26:59 +0800

    chore: implement map function and add increasing triplet subsequence documentation

--- index.ts
+++ index.ts
@@ -1,12 +1,5 @@
-function twoSum(nums: number[], target: number): number[] {
-  const hashmap: Record<number, number> = {};
-  const result: number[] = [];
-
-  for (let i = 0; i < nums.length; i++) {
-    hashmap[i] = nums[i];
-  }
-
-  return result;
+function map(arr: number[], fn: (n: number, i: number) => number): number[] {
+  const output: number[] = [];
+  arr.forEach((n, id) => output.push(fn(n, id)));
+  return output;
 }
-
-console.log(twoSum([2, 7, 11, 15], 9));
