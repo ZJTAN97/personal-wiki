889f5eadf44cc57f8f485e830f7ba9ddc35fe395
ZJ <zhijietan97@gmail.com>
2026-01-06 22:52:53 +0800

    feat: implement findMaxAverage function and add documentation for Maximum Average Subarray 1

--- index.ts
+++ index.ts
@@ -1,15 +1,22 @@
 function findMaxAverage(nums: number[], k: number): number {
-  let windowAverage = nums.slice(0, k).reduce((a, b) => a + b, 0) / k;
-  let maxAverage = windowAverage;
+  if (nums.length === 1) return nums[0];
+
+  let maxAverage = 0;
+
+  let windowAverage = nums.slice(0, k).reduce((a, b) => a + b, 0);
+
+  maxAverage = windowAverage / k;
 
   for (let i = k; i < nums.length; i++) {
-    windowAverage = (windowAverage + nums[i] - nums[i - k]) / k;
-
-    maxAverage = Math.max(maxAverage, windowAverage);
+    windowAverage += nums[i] - nums[i - k];
+    maxAverage = Math.max(maxAverage, windowAverage / k);
   }
+
+  console.log(maxAverage);
 
   return maxAverage;
 }
 
 findMaxAverage([1, 12, -5, -6, 50, 3], 4); // 12.75
 findMaxAverage([5], 1); // 5
+findMaxAverage([3, 3, 4, 3, 0], 3);
