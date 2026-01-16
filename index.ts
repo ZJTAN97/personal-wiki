2fe5d8fda6fed1745778b918cadcc29b1c72087a
ZJ <zhijietan97@gmail.com>
2026-01-16 11:56:56 +0800

    chore: add notes on html elements and rules of react hook

--- index.ts
+++ index.ts
@@ -1,22 +1,11 @@
-function longestOnes(nums: number[], k: number): number {
-  let result = 0;
-  let left = 0;
-  let zeroCount = 0;
+// help me to create this promiseTest that if i executed the code above it will print out 'hello world' after 5 s example: promiseTest.then((res) => console.log(res)); "
 
-  for (let right = 0; right < nums.length; right++) {
-    if (nums[right] === 0) {
-      zeroCount += 1;
-    }
+const promiseTest = async () => {
+  return await new Promise(() =>
+    setTimeout(() => {
+      console.log("hello world");
+    }, 5000)
+  );
+};
 
-    while (zeroCount > k) {
-      if (nums[left] === 0) zeroCount -= 1;
-      left += 1;
-    }
-
-    result = Math.max(result, right - left + 1);
-  }
-
-  return result;
-}
-
-longestOnes([1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0], 2); // 6 [1,1,1,0,0,1*,1,1,1,1,1*]
+promiseTest();
