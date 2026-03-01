ba616b7f559000ee4b699268eec2761f5cc88609
ZJ <zhijietan97@gmail.com>
2026-03-01 19:19:52 +0900

    chore: add string problem

--- index.ts
+++ index.ts
@@ -1,43 +1,40 @@
-class ListNode {
-  val: number;
-  next: ListNode | null;
-  constructor(val?: number, next?: ListNode | null) {
-    this.val = val === undefined ? 0 : val;
-    this.next = next === undefined ? null : next;
+function myAtoi(s: string): number {
+  let index = 0;
+  let result = "";
+  let isNegative = false;
+
+  const UPPER_LIMIT = 2 ** 31 - 1;
+  const LOWER_LIMIT = -(2 ** 31);
+
+  while (s[index] === " ") {
+    index++;
   }
+
+  if (s[index] === "+" || s[index] === "-") {
+    isNegative = s[index] === "-";
+    index++;
+  }
+
+  for (let i = index; i < s.length; i++) {
+    const number = s.charCodeAt(i) - 48; // only takes in 0-9
+
+    if (number < 0 || number > 9) {
+      break;
+    } else {
+      result = result + s[i];
+    }
+  }
+
+  const output = isNegative ? -1 * Number(result) : Number(result);
+
+  return output > UPPER_LIMIT
+    ? UPPER_LIMIT
+    : output < LOWER_LIMIT
+      ? LOWER_LIMIT
+      : output;
 }
 
-function pairSum(head: ListNode | null): number {
-  if (head?.next && !head?.next?.next) {
-    return head.val + head.next.val;
-  }
-
-  const arr = [];
-  let n = 0;
-  let currentNode = head;
-
-  while (currentNode) {
-    n++;
-    arr.push(currentNode.val);
-    currentNode = currentNode.next;
-  }
-
-  let twinSum = 0;
-
-  for (let i = 0; i < arr.length / 2; i++) {
-    twinSum = Math.max(twinSum, arr[i] + arr[n - 1 - i]);
-  }
-
-  return twinSum;
-}
-
-const first = new ListNode(5);
-const second = new ListNode(4);
-const third = new ListNode(2);
-const fourth = new ListNode(1);
-
-first.next = second;
-second.next = third;
-third.next = fourth;
-
-pairSum(first);
+console.log(myAtoi("42"));
+console.log(myAtoi(" -042"));
+console.log(myAtoi("1337c0d3"));
+console.log(myAtoi("words and 987"));
