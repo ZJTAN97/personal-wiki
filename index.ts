ff08467731bcfbeca8dfb8aa3c3615b5c9795367
ZJ <zhijietan97@gmail.com>
2026-03-02 09:47:24 +0900

    chore: update string dsa

--- index.ts
+++ index.ts
@@ -1,40 +1,54 @@
-function myAtoi(s: string): number {
-  let index = 0;
-  let result = "";
-  let isNegative = false;
+function calculate(s: string): number {
+  // initialize stack as empty array
+  const stack: number[] = [];
 
-  const UPPER_LIMIT = 2 ** 31 - 1;
-  const LOWER_LIMIT = -(2 ** 31);
+  // initialize num for adding number and prevOperator to track Math operator
+  let num: string = "";
+  let prevOperator: string = "+";
 
-  while (s[index] === " ") {
-    index++;
-  }
+  // run for loop from index 0 to length of s
+  for (let i = 0; i <= s.length; i++) {
+    // initialize char as s[i]
+    const char: string = s[i];
 
-  if (s[index] === "+" || s[index] === "-") {
-    isNegative = s[index] === "-";
-    index++;
-  }
+    // i char is greater than or equal to "0" and less than or equal to "9"
+    // this handles scenarios like double digits numbers e.g. 13, 15, 42
+    if (char >= "0" && char <= "9") {
+      // add char in num and continue for loop
+      num += char;
+      continue;
+    }
 
-  for (let i = index; i < s.length; i++) {
-    const number = s.charCodeAt(i) - 48; // only takes in 0-9
+    // if char is less than "0" or char is greater than "9" and char is not equal to " " space or i is equal to length of s
+    if (((char < "0" || char > "9") && char !== " ") || i === s.length) {
+      switch (prevOperator) {
+        case "+": {
+          stack.push(Number(num));
+          break;
+        }
+        case "-": {
+          stack.push(-Number(num));
+          break;
+        }
+        case "*": {
+          stack.push(Number(stack.pop()) * Number(num));
+          break;
+        }
+        case "/": {
+          stack.push(Math.trunc(Number(stack.pop()) / Number(num)));
+          break;
+        }
+      }
 
-    if (number < 0 || number > 9) {
-      break;
-    } else {
-      result = result + s[i];
+      // update prevOperator with char and num with "" empty string
+      prevOperator = char;
+      num = "";
     }
   }
 
-  const output = isNegative ? -1 * Number(result) : Number(result);
-
-  return output > UPPER_LIMIT
-    ? UPPER_LIMIT
-    : output < LOWER_LIMIT
-      ? LOWER_LIMIT
-      : output;
+  // do sum of the all the number from stack and return it
+  return stack.reduce((total, value) => total + value, 0);
 }
 
-console.log(myAtoi("42"));
-console.log(myAtoi(" -042"));
-console.log(myAtoi("1337c0d3"));
-console.log(myAtoi("words and 987"));
+console.log(calculate("3+2*2"));
+console.log(calculate(" 3/2 "));
